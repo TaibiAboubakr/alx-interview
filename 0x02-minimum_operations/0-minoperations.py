fe570fee9923421e2a6753bf273cab4e2cdc2c46
aboubakr <c.aboubakr@gmail.com>
2024-04-24 20:08:10 +0100

    add the file 0-minoperations.py

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" min operations """
+
+
+def minOperations(n: int) -> int:
+    """min operations  """
+    if n <= 1:
+        return 0
+    if n == 2:
+        return 2
+    countOp = 2
+    nbrH = 2
+    while nbrH < n:
+        if n % nbrH == 0:
+            oldnbrH = nbrH
+            nbrH *= 2
+            countOp += 2
+        else:
+            tmp = nbrH
+            nbrH += oldnbrH
+            oldnbrH = tmp
+            countOp += 1
+    return countOp
