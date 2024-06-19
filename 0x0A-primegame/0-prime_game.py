05f9f03159829e41333269c5bda84d8f76c2a405
aboubakr <c.aboubakr@gmail.com>
2024-06-19 17:14:54 +0100

    update the file 0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -5,8 +5,8 @@
 def isWinner(x, nums):
     """ isWinner(x, nums) """
 
-    def sieve_of_eratosthenes(max_num):
-        """ sieve_of_eratosthenes """
+    def GetPrimes(max_num):
+        """ GetPrimes """
         is_prime = [True] * (max_num + 1)
         p = 2
         while (p * p <= max_num):
@@ -14,18 +14,16 @@ def isWinner(x, nums):
                 for i in range(p * p, max_num + 1, p):
                     is_prime[i] = False
             p += 1
-
         return is_prime
-
     if x <= 0 or not nums:
         return None
-    
+
     max_n = max(nums)
-    is_primeNumber = sieve_of_eratosthenes(max_n)
-    
+    is_primeNumber = GetPrimes(max_n)
+
     maria_wins = 0
     ben_wins = 0
-    
+
     for n in nums:
         if n == 1:
             ben_wins += 1
@@ -38,7 +36,7 @@ def isWinner(x, nums):
                 ben_wins += 1
             else:
                 maria_wins += 1
-    
+
     if maria_wins > ben_wins:
         return "Maria"
     elif ben_wins > maria_wins:
