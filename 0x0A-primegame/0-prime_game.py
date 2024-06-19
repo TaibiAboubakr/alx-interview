c0b9da7a40aff7c4e8aa7ba997a02577eaeb6d5e
aboubakr <c.aboubakr@gmail.com>
2024-06-19 17:01:14 +0100

    update the file 0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """ is Winner """
 
-
 def isWinner(x, nums):
     """ isWinner(x, nums) """
+
     def sieve_of_eratosthenes(max_num):
+        """ sieve_of_eratosthenes """
         is_prime = [True] * (max_num + 1)
         p = 2
         while (p * p <= max_num):
@@ -18,26 +19,33 @@ def isWinner(x, nums):
             if is_prime[i]:
                 count += 1
             prime_counts[i] = count
-        return prime_counts
+        return prime_counts, is_prime
 
     if x <= 0 or not nums:
         return None
+    
     max_n = max(nums)
-    prime_counts = sieve_of_eratosthenes(max_n)
+    prime_counts, is_prime = sieve_of_eratosthenes(max_n)
+    
     maria_wins = 0
     ben_wins = 0
+    
     for n in nums:
         if n == 1:
             ben_wins += 1
         else:
-            if prime_counts[n] % 2 == 0:
+            primes = []
+            for i in range(2, n + 1):
+                if is_prime[i]:
+                    primes.append(i)
+            if len(primes) % 2 == 0:
                 ben_wins += 1
             else:
                 maria_wins += 1
-
-        if maria_wins > ben_wins:
-            return "Maria"
-        elif ben_wins > maria_wins:
-            return "Ben"
-        else:
-            return None
+    
+    if maria_wins > ben_wins:
+        return "Maria"
+    elif ben_wins > maria_wins:
+        return "Ben"
+    else:
+        return None
